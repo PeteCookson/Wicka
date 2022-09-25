73aa01ef504caaf534bd40d4259b14be1780ba3f
PeteCookson <pgcookson@gmail.com>
2022-09-25 20:35:10 +0000

    Added SKU to product model

--- products/models.py
+++ products/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
