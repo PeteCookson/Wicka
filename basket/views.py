a5ac7850b863c4eea7abe7a2bb201a82d463a383
PeteCookson <pgcookson@gmail.com>
2022-09-27 21:49:11 +0000

    Added notification CSS, shopping bag preview, and additional messages

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -14,7 +14,7 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ Add a quantity of the specified product to the shopping basket """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
@@ -32,6 +32,7 @@ def add_to_basket(request, item_id):
 def remove_from_basket(request, item_id):
     """ Remove a specified product from the shopping basket """
     try:
+        product = get_object_or_404(Product, pk=item_id)
         basket = request.session.get('basket', {})
 
         """if item_id in list(basket.keys()):
@@ -39,9 +40,11 @@ def remove_from_basket(request, item_id):
         else:
             basket[item_id] = quantity"""
         basket.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your basket')
 
         request.session['basket'] = basket
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
