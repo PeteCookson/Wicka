019e878d15b81418dadfeb7e2dde46793135b1c5
PeteCookson <pgcookson@gmail.com>
2022-09-28 20:30:00 +0000

    Added checkout views and tempplates

--- checkout/migrations/0001_initial.py
+++ checkout/migrations/0001_initial.py
@@ -26,7 +26,6 @@ class Migration(migrations.Migration):
                 ('town_or_city', models.CharField(max_length=40)),
                 ('street_address1', models.CharField(max_length=80)),
                 ('street_address2', models.CharField(blank=True, max_length=80, null=True)),
-                ('county', models.CharField(blank=True, max_length=80, null=True)),
                 ('date', models.DateTimeField(auto_now_add=True)),
                 ('delivery_cost', models.DecimalField(decimal_places=2, default=0, max_digits=6)),
                 ('order_total', models.DecimalField(decimal_places=2, default=0, max_digits=10)),
@@ -37,7 +36,6 @@ class Migration(migrations.Migration):
             name='OrderLineItem',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('product_size', models.CharField(blank=True, max_length=2, null=True)),
                 ('quantity', models.IntegerField(default=0)),
                 ('lineitem_total', models.DecimalField(decimal_places=2, editable=False, max_digits=6)),
                 ('order', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='lineitems', to='checkout.order')),
