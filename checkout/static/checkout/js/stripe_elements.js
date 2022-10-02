2ffcbb7c203d95eb899f135e86ff9ffde60e6028
PeteCookson <pgcookson@gmail.com>
2022-10-02 12:35:22 +0000

    Updated webhook handler to handle profiles

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -91,7 +91,7 @@ form.addEventListener('submit', function(ev) {
                     line2: $.trim(form.street_address2.value),
                     city: $.trim(form.town_or_city.value),
                     country: $.trim(form.country.value),
-                    postal_code: $.trim(form.postcode.value),
+                    postcode: $.trim(form.postcode.value),
                 }
             },
         }).then(function(result) {
