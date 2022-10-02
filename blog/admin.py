2cdd68f78e992d66a4760bc687843280d5245152
PeteCookson <pgcookson@gmail.com>
2022-10-02 22:31:07 +0000

    Added blog app, view and templates

---
+++ blog/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import BlogPost, BlogComment
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'content',
+    )
+
+class BlogCommentAdmin(admin.ModelAdmin):
+    list_display = (
+        'author',
+        'content',
+        'posted_date',
+        'post'
+    )
+
+admin.site.register(BlogPost, BlogPostAdmin)
+admin.site.register(BlogComment, BlogCommentAdmin)
