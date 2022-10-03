e7fa016aed21a40dc41bf2e74a430657543f4d4c
PeteCookson <pgcookson@gmail.com>
2022-10-03 22:07:42 +0000

    Deploy to Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn wicka.wsgi:application
--- wicka/settings.py
+++ wicka/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-!r3cjtdt%q1!(mvkoh7np81_)_d*_l+c@y49svofwwmn4!2l*q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [''wicka-hampers.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -116,16 +116,23 @@ LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'wicka.wsgi.application'
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
+
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
