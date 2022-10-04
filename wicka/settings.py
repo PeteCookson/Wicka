d551c79c796b03d9e0940f08830d540e41360ca6
PeteCookson <pgcookson@gmail.com>
2022-10-04 20:59:15 +0000

    Removed secret key and set debug

--- wicka/settings.py
+++ wicka/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!r3cjtdt%q1!(mvkoh7np81_)_d*_l+c@y49svofwwmn4!2l*q'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['wicka-hampers.herokuapp.com', 'localhost']
 
