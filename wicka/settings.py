68a16ca4e9e2da3fca7c48c3b957643c8a90e2d3
PeteCookson <pgcookson@gmail.com>
2022-10-04 20:06:40 +0000

    Deploy to Heroku

--- wicka/settings.py
+++ wicka/settings.py
@@ -8,8 +8,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-import dj_database_url
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-!r3cjtdt%q1!(mvkoh7np81_)_d*_l+c@y49svofwwmn4!2l*q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [''wicka-hampers.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['wicka-hampers.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -122,17 +122,17 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DATABASE_URL' in os.environ:
+if "DATABASE_URL" in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
-    }
+}
 
 
 # Password validation
