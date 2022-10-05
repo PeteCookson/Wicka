3fc9b8de90bedf55ea96ecb47eb292bec962ef81
PeteCookson <pgcookson@gmail.com>
2022-10-05 21:11:47 +0000

    Update to profiles/models.py

--- profiles/models.py
+++ profiles/models.py
@@ -6,7 +6,7 @@ from django.dispatch import receiver
 from django_countries.fields import CountryField
 
 
-class UserProfile(models.Model):
+class UserProfile(models.Model):                
     """
     A user profile model for maintaining default
     delivery information and order history
@@ -19,7 +19,7 @@ class UserProfile(models.Model):
     default_postcode = models.CharField(max_length=20, null=True, blank=True)
     default_country = CountryField(blank_label='Country', null=True, blank=True)
 
-    def __str__(self):
+    def __str__(self):  
         return self.user.username
 
 
