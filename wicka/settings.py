a3d03547d5ac8a17f432a7df32bb62b68c9a8772
PeteCookson <pgcookson@gmail.com>
2022-11-06 09:12:42 +0000

    Debug set

--- wicka/settings.py
+++ wicka/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-# DEBUG = False
+#DEBUG = False
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['wicka-hampers.herokuapp.com', 'localhost']
