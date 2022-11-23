3efd62e87835a622788b2afa63f98f328ce1e163
PeteCookson <pgcookson@gmail.com>
2022-11-23 17:09:59 +0000

    Preparing code for Render

--- wicka/settings.py
+++ wicka/settings.py
@@ -31,6 +31,11 @@ DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['wicka-hampers.herokuapp.com', 'localhost']
 
+# Add Render.com URL to allowed hosts
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 CSRF_TRUSTED_ORIGINS = ['https://wicka-hampers.herokuapp.com',
                         'https://8000-petecookson-wicka-r457agpep62.ws-eu74.gitpod.io']
 
