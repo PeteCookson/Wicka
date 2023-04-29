1b4eab10cc34d8bbc794b1f2e93aeeee4f400d6d
PeteCookson <pgcookson@gmail.com>
2023-04-29 21:34:07 +0000

    Debug set to false

--- wicka/settings.py
+++ wicka/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '*')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEVELOPMENT' in os.environ
-# DEBUG = True
+# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = false
 
 ALLOWED_HOSTS = ['wicka-hampers.onrender.com', 'localhost']
 
